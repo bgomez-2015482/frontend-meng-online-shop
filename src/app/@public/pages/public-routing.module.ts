882ae40b01e82ce720470513f98b50afdc8c0804
Bgomez-2015482 <bgomez-2015482@kinal.edu.gt>
2021-02-17 16:31:06 -0600

    03 - Plantilla base de la tienda terminada

--- src/app/@public/pages/public-routing.module.ts
+++ src/app/@public/pages/public-routing.module.ts
@@ -5,7 +5,7 @@ import { PublicComponent } from './public.component';
 
 const routes: Routes = [
   {path: '', component: PublicComponent, children: [
-    {path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
+    {path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
     {path: 'contact', loadChildren: () => import ('./contact/contact.module').then(m => m.ContactModule)}
   ]}
 ];
